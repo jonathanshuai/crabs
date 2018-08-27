69151d71682472a29406409c5f624d0c36116e6d
Jonathan Shuai <jonathanshuai0@gmail.com>
2018-08-27 11:06:31 -0700

    updated show_crabs

--- crabs/crabs.py
+++ crabs/crabs.py
@@ -1,4 +1,13 @@
-from .api_caller import CrabCaller
+import io
+
+from PIL import Image
+
+from matplotlib import pyplot as plt
+
+import requests
+
+from api_caller import CrabCaller
+
 
 class Crab():
     def __init__(self):
@@ -7,3 +16,10 @@ class Crab():
     def call_crabs(self):
         # cc.call()
         return True
+
+    def show_crabs(self, url_list):
+        for url in url_list:
+            data = requests.get(url).content
+            img = Image.open(io.BytesIO(data))
+            plt.imshow(img)
+            plt.show()
